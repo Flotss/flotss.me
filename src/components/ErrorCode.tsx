9d251e5a0184100237280d662d2522e9457637df
Flotss <manginf54@gmail.com>
2023-12-22 16:40:03 +0100

    refactor(ErrorCode): Add color of title

--- src/components/ErrorCode.tsx
+++ src/components/ErrorCode.tsx
@@ -11,11 +11,11 @@ const ErrorCode = (props: ErrorCodeProps): JSX.Element => {
     const { code, message } = props;
 
     const getColor = (code: string): string => {
-        if (code.startsWith("4")) return "text-red-500";
-        if (code.startsWith("5")) return "text-orange-500";
-        if (code.startsWith("3")) return "text-blue-500";
-        if (code.startsWith("2")) return "text-green-500";
-        return "text-purple-500";
+        if (code.startsWith("4")) return "red";
+        if (code.startsWith("5")) return "orange";
+        if (code.startsWith("3")) return "blue";
+        if (code.startsWith("2")) return "green";
+        return "purple";
     }
 
 
@@ -24,7 +24,7 @@ const ErrorCode = (props: ErrorCodeProps): JSX.Element => {
     return (
         <>
             <StyledBox className="flex flex-col items-center justify-center px-5 sm:px-20 py-5 space-y-5 mx-5 sm:mx-20 mt-5">
-                <Title title={code} className={color} />
+                <Title title={code} className={color} color={color}/>
                 <Divider />
                 <Text className="text-2xl">{message}</Text>
             </StyledBox>
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,12 +3,14 @@ import { Box, Text } from "@chakra-ui/react"
 interface TitleProps {
     title: string
     className?: string
+    color?: string
 }
 
-export default function Title({ title, className }: TitleProps) {
+export default function Title({ title, className, color }: TitleProps) {
     return (
         <Box className="flex items-center justify-center mt-6">
-            <Text className={`text-3xl mdrepo:text-5xl lgrepo:text-7xl font-bold tracking-tighter sm:text-5xl bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500 pb-5 pr-1 ${className}`}>
+            <Text className={`text-3xl mdrepo:text-5xl lgrepo:text-7xl font-bold tracking-tighter sm:text-5xl bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500 pb-5 pr-1 ${className}`} 
+                  color={color}>
                 {title}
             </Text>
         </Box>
