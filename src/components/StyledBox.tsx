d89e484b496647429602f5882a0aafe6ec938b5e
Flotss <manginf54@gmail.com>
2023-12-22 19:31:59 +0100

    ✨ feat(StyledBox): refactor to use custom background color

--- src/components/StyledBox.tsx
+++ src/components/StyledBox.tsx
@@ -1,9 +1,13 @@
 import { Box } from "@chakra-ui/react";
+import { Ref } from "react";
 
 interface StyledBoxProps {
     children: React.ReactNode;
     className?: string;
+    ref?: Ref<HTMLDivElement>;
+    style?: React.CSSProperties;
 }
+
 export const StyledBox = ({ children, className, ...props }: StyledBoxProps) => (
     <Box className={`rounded-3xl bg-[#131312] p-8 ${className}`} {...props}>
         {children}
