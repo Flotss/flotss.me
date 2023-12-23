466508ab204438973f6b73504d6f322dd3f8beb3
Flotss <manginf54@gmail.com>
2023-12-23 01:38:06 +0100

    🛠️ refactor(Document): Add comments for clarity
    Add comments to describe the purpose of the `Document.tsx` file and the structure of the `Document` component for improved clarity.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { Analytics } from '@vercel/analytics/react';
 
+/**
+ * The `Document` component is a custom Next.js component that allows you
+ * to customize the HTML structure of your application's pages.
+ *
+ * @returns {JSX.Element} - The rendered `Document` component.
+ */
 export default function Document() {
   return (
     <Html lang="en">
+      {/* The <Head> component is used to specify metadata and link to external resources */}
       <Head />
+
       <body>
+        {/* The <Main> component is where the main content of your application is rendered */}
         <Main />
+
+        {/* The <Analytics> component is used for analytics tracking (e.g., Vercel Analytics) */}
         <Analytics />
+
+        {/* The <NextScript> component includes Next.js scripts required for the application */}
         <NextScript />
       </body>
     </Html>
