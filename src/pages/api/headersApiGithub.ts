bd4646c54067420f8f9fb87a2c8f8bdbcfd70ef0
Flotss <manginf54@gmail.com>
2023-12-23 01:40:35 +0100

    🔒 chore(headersApiGithub): Add HTTP headers for GitHub API authentication

--- src/pages/api/headersApiGithub.ts
+++ src/pages/api/headersApiGithub.ts
@@ -1,6 +1,11 @@
+/**
+ * HTTP headers configuration for making authenticated requests to the GitHub API.
+ * This includes the necessary authorization token and API version.
+ */
 const headers: any = {
   "Content-Type": "application/json",
   Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
   "X-GitHub-Api-Version": "2022-11-28",
 };
+
 export default headers;
