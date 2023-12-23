57989ef24eeae9e6377f19c1a0e8d34405ec15e0
Flotss <manginf54@gmail.com>
2023-12-23 01:32:43 +0100

    📝 chore(StyledBox): Document StyledBox component

--- src/components/StyledBox.tsx
+++ src/components/StyledBox.tsx
@@ -8,6 +8,17 @@ interface StyledBoxProps {
     style?: React.CSSProperties;
 }
 
+/**
+ * `StyledBox` is a custom component that wraps content within a styled `Box` component.
+ *
+ * @component
+ * @param {StyledBoxProps} props - The component's props.
+ * @param {React.ReactNode} props.children - The content to be rendered within the `StyledBox`.
+ * @param {string} [props.className] - Additional CSS classes to apply to the `StyledBox`.
+ * @param {Ref<HTMLDivElement>} [props.ref] - Ref object to attach to the underlying `div` element.
+ * @param {React.CSSProperties} [props.style] - Additional inline styles to apply to the `StyledBox`.
+ * @returns {JSX.Element} - The rendered `StyledBox` component.
+ */
 export const StyledBox = ({ children, className, ...props }: StyledBoxProps) => {
     return (
         <Box className={`rounded-3xl bg-box-color p-8 ${className}`} {...props}>
