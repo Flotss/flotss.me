2d51dc8f1dfe530b30db3d23a518ff3002e4d8af
Flotss <manginf54@gmail.com>
2023-12-23 01:37:40 +0100

    🛠️ refactor(_app): Add comments and import React
    Add comments to describe the purpose of the `_app.tsx` file and the `App` component.
    Also, import React to ensure compatibility with React components.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Layout from "@/components/layout";
 import "@/styles/globals.css";
 import "@/styles/scrollbar.css";
@@ -5,13 +6,23 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import dotenv from "dotenv";
 
+/**
+ * The `App` component is the entry point for your Next.js application.
+ *
+ * @param {AppProps} props - The properties passed to the `App` component.
+ * @returns {JSX.Element} - The rendered `App` component.
+ */
 export default function App({ Component, pageProps }: AppProps) {
-
+  // Load environment variables from a .env file if available
   dotenv.config();
+
   return (
     <ChakraProvider>
+      {/* Define the overall layout of the application */}
       <div className="flex flex-col min-h-screen bg-[#000000] text-[#F7FAFC]">
+        {/* Render the layout, which includes header and footer */}
         <Layout>
+          {/* Render the main content of the application */}
           <Component {...pageProps} />
         </Layout>
       </div>
