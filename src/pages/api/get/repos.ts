58fd4a1ba41a2712e4d798ccde39dc60dd769f94
Flotss <manginf54@gmail.com>
2023-12-23 02:34:58 +0100

    ✨ refactor(api): Streamline JSON to type conversion in repos.ts

--- src/pages/api/get/repos.ts
+++ src/pages/api/get/repos.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import fetch from "node-fetch";
-import { Collaborator, Commit, Repo } from "@/types/types";
+import { Collaborator, Commit, PullRequest, Repo } from "@/types/types";
 import headers from "../headersApiGithub";
 
 // Define the type of repositories
@@ -92,41 +92,7 @@ async function getRepos(owner: string): Promise<Repo[]> {
   await Promise.all(
     reposResponse.map(async (rep: any) => {
       if (repositories.some((repo) => repo.name === rep.name)) {
-        let repo: Repo = {
-          id: rep.id,
-          name: rep.name,
-          description: rep.description,
-          url: rep.url,
-          html_url: rep.html_url,
-          created_at: rep.created_at,
-          updated_at: rep.updated_at,
-          stars: rep.stargazers_count,
-          archived: rep.archived,
-          language: "",
-          homepage: "",
-          git_url: "",
-          ssh_url: "",
-          clone_url: "",
-          svn_url: "",
-          forked: false,
-          commits: [],
-          readme: "",
-          owner: {
-            login: "",
-            avatar_url: "",
-            url: "",
-            html_url: ""
-          },
-          collaborators: [],
-          languages: [],
-          pullrequests: [],
-          open_issues_count: 0,
-          license: "",
-          subscribers_count: 0,
-          forks_count: 0,
-          watchers_count: 0
-        };
-        repos.push(repo);
+        repos.push(rep as Repo);
       }
     })
   );
@@ -158,52 +124,19 @@ async function getRepo(owner: string, repoName: string): Promise<Repo | null> {
     throw new Error("API rate limit exceeded");
   }
 
-  const commits = await getAllCommits(owner, repoName);
+  
+  const repo: Repo = reponseJson as Repo;
+  
+  repo.commits = await getAllCommits(owner, repoName);
 
-  const repo: Repo = {
-    id: reponseJson.id,
-    name: reponseJson.name,
-    description: reponseJson.description,
-    url: reponseJson.url,
-    html_url: reponseJson.html_url,
-    created_at: reponseJson.created_at,
-    updated_at: reponseJson.updated_at,
-    stars: reponseJson.stargazers_count,
-    archived: reponseJson.archived,
-    language: reponseJson.language,
-    homepage: reponseJson.homepage,
-    git_url: reponseJson.git_url,
-    ssh_url: reponseJson.ssh_url,
-    clone_url: reponseJson.clone_url,
-    svn_url: reponseJson.svn_url,
-    forked: reponseJson.fork,
-    commits: commits,
-    collaborators: [],
-    languages: [],
-    pullrequests: [],
-    open_issues_count: reponseJson.open_issues_count,
-    license: reponseJson.license ? reponseJson.license.name : "",
-    subscribers_count: reponseJson.subscribers_count,
-    forks_count: reponseJson.forks_count,
-    watchers_count: reponseJson.watchers_count,
-    readme: "",
-    owner: {
-      login: "",
-      avatar_url: "",
-      url: "",
-      html_url: ""
-    }
-  };
-
-
-  if (reponseJson.owner) {
-    repo.owner = {
-      login: reponseJson.owner.login,
-      avatar_url: reponseJson.owner.avatar_url,
-      url: reponseJson.owner.url,
-      html_url: reponseJson.owner.html_url,
-    };
-  }
+  // if (reponseJson.owner) {
+  //   repo.owner = {
+  //     login: reponseJson.owner.login,
+  //     avatar_url: reponseJson.owner.avatar_url,
+  //     url: reponseJson.owner.url,
+  //     html_url: reponseJson.owner.html_url,
+  //   };
+  // }
 
   // Retrieve collaborators
   const collaboratorsResponse = await fetch(
@@ -213,17 +146,7 @@ async function getRepo(owner: string, repoName: string): Promise<Repo | null> {
 
   if (collaboratorsResponse.ok) {
     const collaboratorsJson: unknown = await collaboratorsResponse.json();
-    const collaborators: any[] = collaboratorsJson as any[];
-
-    repo.collaborators = collaborators.map((collaborator) => {
-      const collaboratorNew: Collaborator = {
-        login: collaborator.login,
-        avatar_url: collaborator.avatar_url,
-        url: collaborator.url,
-        html_url: collaborator.html_url,
-      };
-      return collaboratorNew;
-    });
+    repo.collaborators = collaboratorsJson as Collaborator[];;
   } else {
     repo.collaborators = [];
   }
@@ -235,7 +158,7 @@ async function getRepo(owner: string, repoName: string): Promise<Repo | null> {
   );
   if (languagesResponse.ok) {
     const languagesJson: unknown = await languagesResponse.json();
-    const languages: any = languagesJson as any;
+    const languages = languagesJson as any;
 
     const total = Object.values(languages).reduce(
       (acc: number, value: unknown) => acc + (value as number),
@@ -270,28 +193,7 @@ async function getRepo(owner: string, repoName: string): Promise<Repo | null> {
 
   if (pullrequestsResponse.ok) {
     const pullrequestsJson: unknown = await pullrequestsResponse.json();
-    const pullrequests: any[] = pullrequestsJson as any[];
-
-    repo.pullrequests = pullrequests.map((pullrequest) => {
-      return {
-        url: pullrequest.url,
-        id: pullrequest.id,
-        node_id: pullrequest.node_id,
-        html_url: pullrequest.html_url,
-        diff_url: pullrequest.diff_url,
-        patch_url: pullrequest.patch_url,
-        issue_url: pullrequest.issue_url,
-        number: pullrequest.number,
-        title: pullrequest.title,
-        user: {
-          login: pullrequest.user.login,
-          avatar_url: pullrequest.user.avatar_url,
-          url: pullrequest.user.url,
-          html_url: pullrequest.user.html_url,
-        },
-        body: pullrequest.body,
-      };
-    });
+    repo.pullrequests = pullrequestsJson as PullRequest[];
   } else {
     repo.pullrequests = [];
   }
