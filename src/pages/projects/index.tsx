4e5ae054b9fea0e84d48ce8121bab8c73f4c54c0
Flotss <manginf54@gmail.com>
2023-12-23 01:41:20 +0100

    ✨ feat(Projects): Create Projects component to display repositories

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,12 +1,20 @@
+import React from "react";
 import Repos from "@/components/Repos";
 import Title from "@/components/Title";
 
+/**
+ * The `Projects` component is responsible for displaying a list of repositories.
+ *
+ * @returns {JSX.Element} - The rendered `Projects` component.
+ */
+export default function Projects() {
+  return (
+    <>
+      {/* Render the page title */}
+      <Title title="Mes projets" />
 
-export default function projects() {
-    return (
-        <>
-            <Title title="Mes projets" />
-            <Repos />
-        </>
-    )
+      {/* Render the list of repositories */}
+      <Repos />
+    </>
+  );
 }
