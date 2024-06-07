53b8a314d6c2a329012401aba5a3c3c9e5222e3f
Flotss <manginf54@gmail.com>
2024-06-07 19:41:59 +0200

    begining of contact

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,5 +1,16 @@
-import WorkInProgress from '@/components/WorkInProgress';
+import { StyledBox } from '@/components/StyledBox';
+import { Heading, Text } from '@chakra-ui/react';
 
-export default function contact() {
-  return <WorkInProgress />;
+export default function contact(props) {
+  return (
+    <div className="flex flex-col gap-2 px-5 py-2 sm:px-20">
+      <StyledBox>
+        <Heading>Contact Page</Heading>
+        <Text>This is the contact page.</Text>
+      </StyledBox>
+      <StyledBox>
+        <Heading>Contact Page</Heading>
+      </StyledBox>
+    </div>
+  );
 }
