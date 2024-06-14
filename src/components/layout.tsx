002bb7a0635acf4157e927dc422607103467f46e
Flotss <manginf54@gmail.com>
2024-06-14 20:29:07 +0200

    Refactor layout component and update project card imports

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -16,19 +16,6 @@ import { Head } from 'next/document';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* The <Head> component is used to specify metadata and link to external resources */}
-      <Head>
-        {/* The <title> element specifies the title of the document */}
-        <title>Flotss.me</title>
-
-        {/* The <meta> element provides metadata about the HTML document */}
-        <meta name="description" content="Flotss personal website" />
-        <meta name="author" content="Flotss" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        {/* The <link> element defines the relationship between a document and an external resource */}
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Header />
       <main>{children}</main>
       <Footer />
