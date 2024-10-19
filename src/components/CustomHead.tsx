7677766033947b43e0c1fece3dfae0324b9a16ae
Florian Mangin <manginf54@gmail.com>
2024-10-20 00:39:45 +0200

    🔧 (seo): Update robots meta tag and improve Open Graph and Twitter metadata
    - Changed the `robots` meta tag to reference `/robots.txt` for better SEO control.
    - Added `og:type` for more specific Open Graph data.
    - Updated Twitter meta tags to use `summary_large_image` for enhanced social media previews.

--- src/components/CustomHead.tsx
+++ src/components/CustomHead.tsx
@@ -6,13 +6,18 @@ const CustomHead = () => {
       {/* Add any other meta tags or customizations here */}
       <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="robots" content="index, follow" />
+      <meta name="robots" content="/robots.txt" />
       <link rel="manifest" href="/manifest.json" />
+
+      {/* Open Graph Meta Tags */}
       <meta property="og:title" content="Flotss.me" />
       <meta property="og:description" content="Flotss portfolio website" />
       <meta property="og:image" content="/site-representation.png" />
       <meta property="og:url" content="https://flotss.me" />
-      <meta name="twitter:card" content="summary" />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter Meta Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@flotss" />
       <meta name="twitter:title" content="Flotss.me" />
       <meta name="twitter:description" content="Flotss portfolio website" />
